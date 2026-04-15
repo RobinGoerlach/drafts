00a7b1b6026626389cca5a37bd8dd495aa23626a
Robin Goerlach <robin.goerlach@sasd.de>
2026-04-15 11:55:51 +0200

    multiple updates in kontakt linksammlung add linux

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
-$lang = $_GET['lang'] ?? null;
-if ($lang === 'de') { header('Location: /de/', true, 302); exit; }
-if ($lang === 'en') { header('Location: /en/', true, 302); exit; }
+
 $acceptLanguage = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '');
 $target = preg_match('/\bde\b|de-/', $acceptLanguage) === 1 ? '/de/' : '/en/';
 header('Vary: Accept-Language');
