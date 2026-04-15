ac4d8555486b0e39808f60ecb6336f459262b5ec
Robin Goerlach <robin.goerlach@sasd.de>
2026-04-15 11:49:19 +0200

    multiple adds in themen

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 declare(strict_types=1);
-
-$languageHeader = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '');
-$target = '/en/';
-
-if (preg_match('/\bde\b|de-/', $languageHeader) === 1) {
-    $target = '/de/';
-}
-
+$lang = $_GET['lang'] ?? null;
+if ($lang === 'de') { header('Location: /de/', true, 302); exit; }
+if ($lang === 'en') { header('Location: /en/', true, 302); exit; }
+$acceptLanguage = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '');
+$target = preg_match('/\bde\b|de-/', $acceptLanguage) === 1 ? '/de/' : '/en/';
 header('Vary: Accept-Language');
 header('Location: ' . $target, true, 302);
 exit;
